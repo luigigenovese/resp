b251aa61a78d879f5836c3a9097135dab5f5612d
William Dawson <william.dawson@riken.jp>
2017-11-29 13:29:09 +0900

    Basic routines from my sparse matrix library.
    I have left out some subroutines that I need since we would like to reimagine
    those as iterators.

--- resp.f90
+++ resp.f90
@@ -2,12 +2,12 @@
 !!   SparseLib matrix operations
 !! @author
 !!   Copyright (C) 2017 SparseLib developers
-!!   
+!!
 !!   SparseLib is free software: you can redistribute it and/or modify
 !!   it under the terms of the GNU Lesser General Public License as published by
 !!   the Free Software Foundation, either version 3 of the License, or
 !!   (at your option) any later version.
-!!   
+!!
 !!   CheSS is distributed in the hope that it will be useful,
 !!   but WITHOUT ANY WARRANTY; without even the implied warranty of
 !!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -28,6 +28,7 @@ module resp
 
   contains
 
+    !! Constructors
     function resp_matrix_create(dict_options) result(sm)
       !initialize the resp_matrix object, that might be filled and allocated afterwards
       implicit none
@@ -36,9 +37,73 @@ module resp
     end function resp_matrix_create
 
     subroutine resp_matrix_set(sm)
-      !allocate the memory 
+      !allocate the memory
       type(resp_matrix) :: sm !matrix to be allocated, should be initialized first
     end subroutine resp_matrix_set
+
+    subroutine resp_matrix_copy(sm) result(smcopy)
+      ! A copy constructor for matrices
+      type(resp_matrix), intent(in) :: sm ! input matrix to copy
+      type(resp_matrix), intent(inout) :: smcopy
+    end subroutine resp_matrix_copy
+
+    subroutine resp_matrix_destroy(sm)
+      ! A destructor for matrices
+      type(resp_matrix), intent(inout) :: sm ! input matrix to copy
+    end subroutine resp_matrix_destroy
+
+    !! I/O Routines
+    subroutine resp_matrix_readcheckpoint(sm, file_name)
+      ! Some sort of routine that reads an internal file
+      ! representation from disk as efficiently as possible for checkpointing.
+      ! In NTPoly this reads a binary file.
+      type(resp_matrix), intent(inout) :: sm ! input matrix to write
+      character(len=*), intent(in) :: file_name ! name of the file to read
+    end subroutine resp_matrix_readcheckpoint
+
+    subroutine resp_matrix_writecheckpoint(sm, file_name)
+      ! Some sort of routine that writes an internal file
+      ! representation to disk as efficiently as possible for checkpointing.
+      ! In NTPoly this writes a binary file.
+      type(resp_matrix), intent(in) :: sm ! input matrix to write
+      character(len=*), intent(in) :: file_name ! name of the file to write
+    end subroutine resp_matrix_writecheckpoint
+
+    subroutine resp_matrix_readmatrixmarket(sm, file_name)
+      ! Read the matrix from file using the matrix market format
+      type(resp_matrix), intent(inout) :: sm ! input matrix to write
+      character(len=*), intent(in) :: file_name ! name of the file to write
+    end subroutine resp_matrix_readmatrixmarket
+
+    subroutine resp_matrix_writetomatrixmarket(sm, file_name)
+      ! Write the matrix to file using the matrix market format
+      type(resp_matrix), intent(in) :: sm ! input matrix to write
+      character(len=*), intent(in) :: file_name ! name of the file to write
+    end subroutine resp_matrix_writetomatrixmarket
+
+    !! Conversion Routines
+    subroutine resp_matrix_fromtripletlist(sm, triplet_list)
+      ! Fill in the values of the matrix using a list of triplets:
+      ! (row, column, value). This is like the coo_matrix constructor in `SciPy
+      ! <https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.coo_matrix.html>`_
+      type(resp_matrix), intent(in) :: A ! input matrix to convert
+      type(triplet_list), intent(out) :: triplet_list ! output list
+    end subroutine resp_matrix_fromtripletlist
+
+    !! Operations
+    subroutine resp_axpy(A, B, alpha)
+      ! Increment a matrix.
+      ! B = alpha*A + B
+      type(resp_matrix), intent(in) :: A ! Matrix To Add
+      type(resp_matrix), intent(inout) :: B ! Matrix to accumulate in
+      real(fp), intent(in) :: alpha ! Scaling factor
+    end subroutine resp_axpy
+
+    function resp_dot(A, B) result(value)
+      ! Compute the dot product of two matrices.
+      type(resp_matrix), intent(in) :: A, B ! Matrices to dot together
+      real(fp) :: value
+    end function resp_dot
 
     subroutine resp_gemm(A,B,C)
       !perform a matrix-matrix multiplication
@@ -47,6 +112,39 @@ module resp
       type(resp_matrix), intent(inout) :: C !output matrix, should have bes set first
     end subroutine resp_gemm
 
+    function resp_norm(sm) result(value)
+      ! Compute the norm of a matrix.
+      ! Maybe there should be a parameter to specify which norm?
+      type(resp_matrix), intent(in) :: sm ! Matrix to compute the norm of.
+      real(fp) :: value
+    end function resp_norm
+
+    subroutine resp_scal(sm, alpha)
+      ! Scale a matrix by a constant.
+      ! A = A*alpha
+      type(resp_matrix), intent(in) :: sm ! Matrix To Scale
+      real(fp), intent(in) :: alpha ! Scaling factor
+    end subroutine resp_scal
+
+    function resp_trace(sm) result(value)
+      ! Compute the trace of a matrix.
+      type(resp_matrix), intent(in) :: sm ! Matrix to compute the trace of.
+      real(fp) :: value
+    end function resp_trace
+
+    function resp_transpose(A) result(AT)
+      ! Compute the transpose of a matrix
+      type(resp_matrix), intent(in) :: A ! input matrix to transpose
+      type(resp_matrix) :: AT
+    end function resp_transpose
+
+    !! Basic Data Access
+    function resp_matrix_getdescriptor(sm) result(data)
+      ! Get a dictionary which describes the features of this matrix.
+      ! For example: rows, columns, non-zero entries, symmetry, etc.
+      type(resp_matrix), intent(in) :: sm ! matrix to get information about
+      type(dictionary), pointer :: data
+    end function resp_matrix_getdescriptor
+
+
 end module resp
-
-
--- source/sparsematrix.rst
+++ source/sparsematrix.rst
@@ -1,12 +1,12 @@
 Sparse Matrix Operations API
 =======================
 
-In this file we will put out proposition for the API of the 
+In this file we will put out proposition for the API of the
 sparsematrix operations to be performed.
 We will profit of this file also to learn the good prectice to create
 developer's documentation for the library
 
-Basic types 
+Basic types
 -----------
 
 .. f:type:: resp/resp_matrix
@@ -14,7 +14,7 @@ Basic types
    Basic entity of the library, represents a Matrix with a defined sparsity level
    Here we list the important attributes that we would like to expose
 
-   :f integer ndummy: Placehoder for some componenets of the structure we would like to expose
+   :f integer ndummy: Placeholder for some components of the structure we would like to expose
 
 Constructors
 ------------
@@ -26,20 +26,70 @@ We here indicate the basic routines for the construction of the derived types
 
 .. f:autofunction:: resp_matrix_set
 
+.. f:autofunction:: resp_matrix_copy
+
+.. f:autofunction:: resp_matrix_destroy
+
+I/O Routines
+------------
+Here we indicate the basic routines for performing input and output operations.
+
+.. f:autofunction:: resp_matrix_readcheckpoint
+
+.. f:autofunction:: resp_matrix_writecheckpoint
+
+The following two routines are for processing the `Matrix Market Format
+<http://math.nist.gov/MatrixMarket/formats.html#MMformat>`_.
+
+.. f:autofunction:: resp_matrix_readmatrixmarket
+
+.. f:autofunction:: resp_matrix_writetomatrixmarket
+
+Conversion Routines
+------------
+Here we indicate the basic routines for converting a matrix (to and) from
+different types.
+
+.. f:autofunction:: resp_matrix_fromtripletlist
 
 Operations
 ----------
+Here we indicate the basic linear algebra routines that can be performed using
+a matrix.
+
+.. f:autosubroutine:: resp_axpy
+
+.. f:autosubroutine:: resp_dot
+
 .. f:autosubroutine:: resp_gemm
 
+.. f:autosubroutine:: resp_norm
+
+.. f:autosubroutine:: resp_scal
+
+.. f:autosubroutine:: resp_trace
+
+.. f:autosubroutine:: resp_transpose
+
+Basic Data Access
+----------
+Here we indicate some helper routines for getting information about the matrix.
+
+.. f:autosubroutine:: resp_matrix_getdescriptor
+
+Scratch Space
+----------
+(For testing manual documentation)
+
 .. This is the example for a manually documented function
-.. 
+..
 .. .. f:function:: yaml_strings/combine_strings
-.. 
+..
 .. 	Function that combines two strings
-.. 
+..
 .. 	:p char a: First argument
 .. 	:p character b: Second argument
-.. 
+..
 
 Example of usage of the library
 =======================
